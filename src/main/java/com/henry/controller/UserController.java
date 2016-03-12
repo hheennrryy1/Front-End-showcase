428a8a5eb51e1ffa4d8b73d26369cb485f765046
hheennrryy1 <hheennrryy1@icloud.com>
2016-03-12 16:55:15 +0800

    update

--- src/main/java/com/henry/controller/UserController.java
+++ src/main/java/com/henry/controller/UserController.java
@@ -2,12 +2,17 @@ package com.henry.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	@RequestMapping("/hello")
-	public String hello() {
-		return "hello";
+	int i = 0;
+	@RequestMapping(value="/testAJAX", method=RequestMethod.POST)
+	@ResponseBody
+	public String testAJAX() {
+		System.out.println(++i);
+		return "test";
 	}
 }
